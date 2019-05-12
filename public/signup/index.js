f25aff825b9760a8d8bb321b6869889acbcf1a0f
rhmt <rezaeihazara@gmail.com>
2019-05-12 21:07:49 +0900

    feat: sign up

---
+++ public/signup/index.js
@@ -0,0 +1,27 @@
+const main = () => {
+  console.log('main loaded')
+  const inputEmail = document.getElementById('email');
+  const inputPassword = document.getElementById('password');
+  const button = document.getElementById('button');
+  
+  const onSignUp = () => {
+    console.log(firebase)
+    const email = inputEmail.value;
+    const password = inputPassword.value;
+    console.log(email, password, 'email and password');
+    
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(res => {
+        console.log('login')
+        window.location.replace('../login/login.html');
+      })
+      .catch(err => {
+        console.log('error: ', err.message)
+    })
+  }
+  
+  button.addEventListener('click', onSignUp)
+}
+
+
+window.addEventListener('DOMContentLoaded', main)
