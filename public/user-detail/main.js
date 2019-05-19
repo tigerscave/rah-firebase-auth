45c75c1d793870b3a9c85cbe6d796c9e94969a73
rhmt <rezaeihazara@gmail.com>
2019-05-19 19:13:38 +0900

    feat: add first name

--- public/user-detail/main.js
+++ public/user-detail/main.js
@@ -6,19 +6,21 @@
 const onCheckState = () => {
   const db = firebase.firestore();
   const biography = document.getElementById('biography');
-  const nameElement = document.getElementById('firstName');
+  const displayName = document.getElementById('displayName');
+  const firstNameElement = document.getElementById('firstName')
   const lastNameElement = document.getElementById('lastName');
   const birthDayElement = document.getElementById('birthDay');
   
   firebase.auth().onAuthStateChanged(user => {
     console.log(user, 'user')
     if(user) {
-      nameElement.innerHTML = user.displayName;
+      displayName.innerHTML = user.displayName;
       
       db.collection('users').doc(user.uid).get().then(doc => {
         console.log(doc.data())
         const userDetail = doc.data();
-        lastNameElement.innerHTML = userDetail.lastName;
+        firstNameElement.innerHTML = userDetail.firstName;
+        lastNameElement.innerHTML = userDetail.lastName
         birthDayElement.innerHTML = userDetail.birthDay;
         biography.innerHTML = userDetail.biography;
       })
@@ -29,19 +31,6 @@ const onCheckState = () => {
   })
 }
 
-const onHogeClicked = () => {
-  console.log("hoge")
-  const db = firebase.firestore();
-  const user = firebase.auth().currentUser;
-  console.log(user.uid);
-  
-  db.collection('users').doc(user.uid).set({
-    firstName: "hoge",
-    secondName: "rah"
-  }).then((res) => {
-    console.log(res)
-  })
-}
 
 const main = () => {
   console.log('file loaded')
@@ -51,7 +40,6 @@ const main = () => {
   editDetail.addEventListener('click', () => {
     window.location.href = '../user-edit-detail'
   })
-  document.getElementById('hoge').addEventListener('click', onHogeClicked)
 }
 
 window.addEventListener('DOMContentLoaded', main)
--- public/user-edit-detail/main.js
+++ public/user-edit-detail/main.js
@@ -1,4 +1,20 @@
 
+const onCheckState = () => {
+  
+  const db = firebase.firestore();
+  firebase.auth().onAuthStateChanged(user => {
+    db.collection('users').doc(user.uid).get().then(doc => {
+      const { firstName, lastName, birthDay, biography } = doc.data()
+      console.log(firstName, 'first name')
+      document.getElementById('firstName').value = firstName;
+      document.getElementById('lastName').value = lastName;
+      document.getElementById('birthDay').value = birthDay;
+      document.getElementById('biography').value = biography
+    })
+  })
+}
+
+
 const onUpdateUserDetail = () => {
   const db = firebase.firestore();
   const user = firebase.auth().currentUser;
@@ -6,14 +22,15 @@ const onUpdateUserDetail = () => {
   const biography = document.getElementById('biography').value;
   const lastName = document.getElementById('lastName').value;
   const birthDay = document.getElementById('birthDay').value;
+  const firstName = document.getElementById('firstName').value;
   
   db.collection('users').doc(user.uid).set({
     biography,
+    firstName,
     lastName,
     birthDay
   }).then(() => {
     window.location.href = '../user-detail'
-    
   })
 }
 
@@ -21,7 +38,7 @@ const onUpdateUserDetail = () => {
 
 const main = () => {
   console.log('file loaded')
-  
+  onCheckState()
   const saveButton = document.getElementById('button');
   saveButton.addEventListener('click', onUpdateUserDetail)
 }
